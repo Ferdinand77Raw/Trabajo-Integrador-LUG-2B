964f26170fd7943ca729cfd1c5ae2e4996e44712
FernandoPavon <max9977raw@gmail.com>
2025-10-23 22:34:11 -0300

    Primer commit, se establecen las capas del proyecto

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPracticoIntegrador_LUG_2B
+{
+    public class Turno
+    {
+    }
+}
